c09d3af0e92213df35b769a61192584cc8335f8b
Jayant Singh Jakhar <jayantsinghjakhar@gmail.com>
2026-08-24 19:54:26 +0530

    Update AuthContext.tsx

--- healthcare_manager/frontend/src/context/AuthContext.tsx
+++ healthcare_manager/frontend/src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState } from 'react';
 import type { ReactNode } from 'react';
 
 type User = { id: string; role: string; name: string };
